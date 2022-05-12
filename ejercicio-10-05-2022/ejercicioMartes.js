0ccb57c5657692e5276121a995397a29018214f2
ante-rj <antequerarjemail.com>
2022-05-11 21:36:29 -0400

    estos son los ejercicios de mayo del 2022

---
+++ ejercicio-10-05-2022/ejercicioMartes.js
@@ -0,0 +1,13 @@
+
+let a = 6
+let b = 4
+
+function decimales (a, b) {
+    (a / b)
+    if (a === 0 ) {
+        console.log(`La division entre A y B es N y es un numero Entero. Operacion exitosa` )
+    }else{
+        `La division entre A y B es N y es un numero decimal. Operacion fallida`
+    }
+
+}
---
+++ ejercicio-11-05-2022/ej-11-05-2022.js
@@ -0,0 +1,7 @@
+const str1 = "hola"
+const str2 = " amigos"
+
+let STR1 = str1.toUpperCase()
+let STR2 = str2.toUpperCase()
+ 
+alert(STR1 + STR2)
